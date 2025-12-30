3db9ddf660c4d3ecefc1ebd717a935d0fa36b27a
Aqif Lohja <aqif.lohja@stu.fbu.edu.tr>
2025-12-31 00:32:44 +0300

    test: add unit tests for database connection and CRUD

---
+++ tests/test_student.cpp
@@ -0,0 +1,15 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+#include "../include/db.hpp"
+#include "../include/StudentManager.h"
+
+TEST_CASE("Database connection works", "[database]") {
+    Database db("dbname=student_db user=student_user password=password123 host=localhost port=5432");
+    REQUIRE(db.getConnection()->is_open() == true);
+}
+
+TEST_CASE("Add student works", "[student]") {
+    Database db("host=localhost port=5432 dbname=sis user=postgres password=yourpassword");
+    StudentManager sm(&db);
+    REQUIRE_NOTHROW(sm.addStudent(1, "John", "Doe", "CS", "john@example.com"));
+}
