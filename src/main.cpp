aad71c45ea21e2697a7d351de1f5a133e31cc5d5
Aqif Lohja <aqif.lohja@stu.fbu.edu.tr>
2025-12-31 01:34:06 +0300

    fix: docker-compose db connectivity and healthcheck

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "db.hpp"
 
 int main() {
-    Database db("host=localhost port=5432 dbname=sis user=postgres password=yourpassword");
+    Database db("host=db port=5432 dbname=sis_db user=sis_user password=sis_pass");
     return 0;
 }
